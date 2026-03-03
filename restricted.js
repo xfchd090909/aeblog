35d9358a521dd2116fae8c3185e2b7a4833b7d8e
aeweb <xfchd090909@gmail.com>
2026-03-03 19:47:26 +0800

    Create restricted.js

---
+++ restricted.js
@@ -0,0 +1,11 @@
+const RESTRICTED_COUNTRIES = [
+  "AU", "Australia", "MY", "Malaysia", "SG", "Singapore",
+  "DE", "Germany",
+  "AT", "Austria", "BE", "Belgium", "BG", "Bulgaria", "HR", "Croatia",
+  "CY", "Cyprus", "CZ", "Czech Republic", "Czechia", "DK", "Denmark",
+  "EE", "Estonia", "FI", "Finland", "FR", "France", "GR", "Greece",
+  "HU", "Hungary", "IE", "Ireland", "IT", "Italy", "LV", "Latvia",
+  "LT", "Lithuania", "LU", "Luxembourg", "MT", "Malta", "NL", "Netherlands",
+  "PL", "Poland", "PT", "Portugal", "RO", "Romania", "SK", "Slovakia",
+  "SI", "Slovenia", "ES", "Spain", "SE", "Sweden"
+];
